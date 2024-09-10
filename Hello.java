8fdfef9089083ae2a5eaca8f09de2550048fad3b
Collin Thom <thomc4564@my.uwstout.edu>
2024-09-09 23:10:01 -0500

    Added Branch Features

--- Hello.java
+++ Hello.java
@@ -4,7 +4,9 @@ public class Hello {
     public static void main(String[] args) {
         String x = "Hello World.";
         String y = "Hello again!";
+        String z = "This is a branch test.";
         System.out.println(x);
         System.out.println(y);
+        System.out.println(z);
     }
 }
