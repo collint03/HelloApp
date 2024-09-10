40b698de5b5da8778be4851d23eb6fc3baae58d0
Collin Thom <thomc4564@my.uwstout.edu>
2024-09-09 23:07:35 -0500

    Added Features

--- Hello.java
+++ Hello.java
@@ -3,6 +3,8 @@ public class Hello {
 
     public static void main(String[] args) {
         String x = "Hello World.";
+        String y = "Hello again!";
         System.out.println(x);
+        System.out.println(y);
     }
 }
